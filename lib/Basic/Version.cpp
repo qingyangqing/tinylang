1fafa31475dab15f9652832666f7af1510a7f8bd
qyang <qingyang.pitt@gmail.com>
2022-07-04 22:42:32 -0700

    Add src files

---
+++ lib/Basic/Version.cpp
@@ -0,0 +1,6 @@
+#include "tinylang/Basic/Version.h"
+
+std::string tinylang::getTinylangVersion() {
+  return TINYLANG_VERSION_STRING;
+}
+
---
+++ tools/driver/Driver.cpp
@@ -0,0 +1,11 @@
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/raw_ostream.h"
+#include "tinylang/Basic/Version.h"
+
+int main(int argc_, const char **argv_) {
+  llvm::InitLLVM X(argc_, argv_);
+  llvm::outs() << "Hello, I'm Tinylang "
+               << tinylang::getTinylangVersion()
+               << "\n";
+}
+
